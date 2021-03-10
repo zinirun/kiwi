73b9bead5155f8dbf583d3f64b51682dc59e345c
Jeongm1n <qll4359@naver.com>
2021-03-10 21:13:36 +0900

    Add: fronted files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Router, Route } from 'react-router';
 
 import { theme } from './common/theme/global';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { ApolloProvider } from 'react-apollo';
 import { AplClient as client } from './configs/apollo';
+
+// 학과공지
+
+// 커뮤니티
+
+// Q&A
+
+// 재능기부
+
+// 장터
+
+// 나의 글
+
+// 나의 댓글
 
 ReactDOM.render(
     <MuiThemeProvider theme={theme}>
