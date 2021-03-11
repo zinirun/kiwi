8f0ccf1b2ae3be94e556a81d1d00494bc96c4f2e
zinirun <heojj97@naver.com>
2021-03-11 17:20:58 +0900

    add-pkg: antd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'antd/dist/antd.css';
 
 import { theme } from './common/theme/global';
 import { MuiThemeProvider } from '@material-ui/core/styles';
