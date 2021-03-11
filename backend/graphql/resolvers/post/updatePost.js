37c09e47accd2ffdc1a53b1bd706da1e335a9ad3
ukunV <showklee@naver.com>
2021-03-11 21:59:33 +0900

    add: updatePost

---
+++ backend/graphql/resolvers/post/updatePost.js
@@ -0,0 +1,40 @@
+/**
+ * 게시물 수정
+ * @author 이건욱
+ * @param input PostUpdateInput {
+        id: ID!
+        title: String!
+        content: String!
+    }
+ * type Post {
+        id: ID!
+        boardId: ID!
+        categoryId: ID!
+        authorId: ID!
+        title: String!
+        content: String!
+        isDeleted: Int!
+        createdAt: Date!
+        updatedAt: Date
+    }
+* updatePost(post: PostUpdateInput!): Boolean
+ */
+
+const models = require('../../../models');
+const { ConflictError } = require('../../errors/errors');
+
+module.exports = async ({ post }, { id: authorId }) => {
+    return await models.Post.update(
+        {
+            title: post.title,
+            content: post.content,
+        },
+        { where: { id: post.id, authorId } },
+    )
+        .then(() => {
+            return true;
+        })
+        .catch(() => {
+            throw ConflictError('Update error occured');
+        });
+};
