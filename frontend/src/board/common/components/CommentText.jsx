81489fe79eb39e118a32ead3d94d4c1938887293
Jeongm1n <qll4359@naver.com>
2021-03-15 20:27:34 +0900

    Revert "remove: Anonymous"
    This reverts commit bc926d16e85c7ce82e71004ecf3c0532962c26cf.

--- frontend/src/board/common/components/CommentText.jsx
+++ frontend/src/board/common/components/CommentText.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import { Button, FormControlLabel, Checkbox } from '@material-ui/core';
 import 'antd/dist/antd.css';
 import { Form, Input, Space } from 'antd';
 import { useStyles } from '../styles/commentText.style';
 const { TextArea } = Input;
+
+const GreenCheckbox = withStyles({
+    root: {
+        color: green[400],
+        '&$checked': {
+            color: green[600],
+        },
+    },
+    checked: {},
+})((props) => <Checkbox color="default" {...props} />);
 
 export default function CommentText() {
     const classes = useStyles();
@@ -16,6 +28,7 @@ export default function CommentText() {
             <Form.Item>
                 <div className={classes.addCommentSection}>
                     <Space size={0}>
+                        <FormControlLabel control={<GreenCheckbox />} label="익명" />
                         <Button type="submit" className={classes.button}>
                             댓글달기
                         </Button>
