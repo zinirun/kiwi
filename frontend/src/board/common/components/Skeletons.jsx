ed76df03fc673fa68bfe2a1d43944ef974b6c322
zinirun <heojj97@naver.com>
2021-03-19 16:36:24 +0900

    modify: darkmode, skeletons

--- frontend/src/board/common/components/Skeletons.jsx
+++ frontend/src/board/common/components/Skeletons.jsx
@@ -14,27 +14,12 @@ export function BoardListSkeleton() {
 
 export function BoardMainSkeleton() {
     return (
-        <>
-            <Grid container>
-                <Grid item xs={12} sm={6}>
+        <Grid container>
+            {[0, 1, 2, 3, 4, 5].map((i) => (
+                <Grid key={i} item xs={12} sm={6}>
                     <Skeleton active paragraph={{ rows: 5 }} />
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Skeleton active paragraph={{ rows: 5 }} />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Skeleton active paragraph={{ rows: 5 }} />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Skeleton active paragraph={{ rows: 5 }} />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Skeleton active paragraph={{ rows: 5 }} />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Skeleton active paragraph={{ rows: 5 }} />
-                </Grid>
-            </Grid>
-        </>
+            ))}
+        </Grid>
     );
 }
