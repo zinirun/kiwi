aafe9d6ff13ec9ffbb137ac5e5c5e0f862dab9dd
zinirun <heojj97@naver.com>
2021-03-22 15:36:17 +0900

    modify: counts handlers

--- backend/graphql/resolvers/post/getPostsCountByBoardId.js
+++ backend/graphql/resolvers/post/getPostsCountByBoardId.js
@@ -4,20 +4,7 @@ const { ConflictError } = require('../../errors/errors');
 module.exports = async ({ boardId, categoryId }, { departmentId }) => {
     const query = `select p.id, COUNT(p.id) AS postsCount
     from post p
-        left join category cg on p.categoryId = cg.id
-        left join (select c.id, count(c.id) as commentCount, postId
-                    from comment c
-                    where c.isDeleted = 0
-                    group by c.postId) as z on p.id = z.postId
-        left join (select pl.id, count(pl.id) as postLikeCount, postId
-                    from post_like pl
-                    where pl.isDeleted = 0
-                    group by pl.postId) as v on p.id = v.postId,
-        user u
-            left join company c on u.companyId = c.id
-            left join grade g on u.studentGradeId = g.id
-    where p.authorId = u.id
-    and p.isDeleted = 0
+    where p.isDeleted = 0
     and p.boardId = :boardId
     and p.departmentId = :departmentId
     ${categoryId && `and p.categoryId=:categoryId`}
--- backend/graphql/resolvers/post/getPostsCountByLikeCount.js
+++ backend/graphql/resolvers/post/getPostsCountByLikeCount.js
@@ -5,23 +5,13 @@ module.exports = async ({ likeCount }, { departmentId }) => {
     console.log(likeCount);
     const query = `SELECT p.id, COUNT(p.id) AS postsCount
     from post p
-        left join category cg on p.categoryId = cg.id
-        left join (select c.id, count(c.id) as commentCount, postId
-                    from comment c
-                    where c.isDeleted = 0
-                    group by c.postId) as z on p.id = z.postId
         left join (select pl.id, count(pl.id) as postLikeCount, postId
                     from post_like pl
                     where pl.isDeleted = 0
-                    group by pl.postId) as v on p.id = v.postId,
-        user u
-            left join company c on u.companyId = c.id
-            left join grade g on u.studentGradeId = g.id
-    where p.authorId = u.id
-    and p.isDeleted = 0
+                    group by pl.postId) as v on p.id = v.postId
+    where p.isDeleted = 0
     and v.postLikeCount >= :likeCount
     and p.departmentId = :departmentId
-    order by p.id desc;
     `;
     return await models.sequelize
         .query(query, {
