680acb139f215f37b9399632e062817ab8c7cd67
zinirun <heojj97@naver.com>
2021-03-22 16:18:20 +0900

    modify: remove DI

--- backend/graphql/resolvers/post/getSearchPostsCount.js
+++ backend/graphql/resolvers/post/getSearchPostsCount.js
@@ -5,23 +5,10 @@ module.exports = async ({ boardId, searchValue }, { departmentId }) => {
     const query = `
                     select p.id, count(p.id) as postsCount
                     from post p
-                        left join (select c.id, count(c.id) as commentCount, postId
-                                    from comment c
-                                    where c.isDeleted = 0
-                                    group by c.postId) as z on p.id = z.postId
-                        left join (select pl.id, count(pl.id) as postLikeCount, postId
-                                    from post_like pl
-                                    where pl.isDeleted = 0
-                                    group by pl.postId) as v on p.id = v.postId,
-                        user u
-                            left join company c on u.companyId = c.id
-                            left join grade g on u.studentGradeId = g.id
-                    where p.authorId = u.id
-                    and p.isDeleted = 0
+                    where p.isDeleted = 0
                     and p.boardId = :boardId
                     and p.departmentId = :departmentId
-                    and title LIKE :searchValue
-                    order by p.id desc;
+                    and p.title LIKE :searchValue;
                     `;
     return await models.sequelize
         .query(query, {
