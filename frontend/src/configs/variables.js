5659b79543620b777f7d247b515622463e4e2b56
zinirun <heojj97@naver.com>
2021-03-22 19:44:03 +0900

    modify: components

--- frontend/src/configs/variables.js
+++ frontend/src/configs/variables.js
@@ -1,4 +1,4 @@
-export const TOP_BOARD_LIKE_COUNT = 1;
+export const TOP_BOARD_LIKE_COUNT = 0;
 export const CONTENT_MAX_WIDTH = 850;
 export const ITEMS_COUNT_PER_PAGE = 12;
 export const CONTENT_FILE_UPLOAD_MAX_COUNT = 3;
