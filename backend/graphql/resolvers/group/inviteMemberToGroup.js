f13b40cb02c68b1c258f927fd77c44b5c312fcd7
zinirun <heojj97@naver.com>
2021-03-23 16:36:52 +0900

    add: Group Resolvers

---
+++ backend/graphql/resolvers/group/inviteMemberToGroup.js
@@ -0,0 +1,38 @@
+/**
+ * 그룹 멤버 초대
+ * @author zini
+ * @param {groupId, memberId}
+ * @return {Boolean}
+ * @resolver inviteGroupMember(groupId: ID!, memberId: ID!): Boolean
+ */
+
+const models = require('../../../models');
+const { ConflictError, BadRequestError } = require('../../errors/errors');
+
+module.exports = async ({ groupId, memberId }, { id: masterId }) => {
+    const isMaster = await models.groups.findOne({
+        attributes: ['id', 'masterId', 'groupId'],
+        where: { masterId, id: groupId },
+        raw: true,
+    });
+    if (!isMaster) {
+        throw BadRequestError('Bad Request: Not Master');
+    }
+    const isAlreadyExists = await models.group_member.findOne({
+        attributes: ['id', 'memberId', 'groupId'],
+        where: { memberId, groupId },
+        raw: true,
+    });
+    if (isAlreadyExists) {
+        throw BadRequestError('Bad Request: Already Member');
+    }
+    return await models.group_member
+        .create({
+            groupId,
+            memberId,
+        })
+        .then(() => true)
+        .catch(() => {
+            throw ConflictError('Conflict error occured at Create');
+        });
+};
