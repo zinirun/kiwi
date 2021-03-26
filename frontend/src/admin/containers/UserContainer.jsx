85f851d3b41599298b53a5a11f7852d0b411c13b
Jeongm1n <qll4359@naver.com>
2021-03-26 22:05:57 +0900

    Update: admin/user

--- frontend/src/admin/containers/UserContainer.jsx
+++ frontend/src/admin/containers/UserContainer.jsx
@@ -1,10 +1,33 @@
 import React, { useState } from 'react';
-import { Input, Row, Col, message } from 'antd';
+import { Input, Row, Col, message, Button } from 'antd';
 import { Grid } from '@material-ui/core';
 import { useMutation } from 'react-apollo';
 import { SEARCH_USER_BY_USER_ID } from '../../configs/queries';
-import { useStyles } from '../styles/user.style';
+import { useStyles } from '../styles/admin.style';
 const { Search } = Input;
+
+const buttonData = [
+    {
+        type: 0,
+        buttonName: '일반회원',
+    },
+    {
+        type: 1,
+        buttonName: '학생회장',
+    },
+    {
+        type: 2,
+        buttonName: '부학생회장',
+    },
+    {
+        type: 3,
+        buttonName: '과사무실',
+    },
+    {
+        type: 9,
+        buttonName: '관리자',
+    },
+];
 
 export default function UserContainer() {
     const classes = useStyles();
@@ -53,28 +76,49 @@ export default function UserContainer() {
                 </Grid>
             </Grid>
             {userInfo && (
-                <Row gutter={[12, 12]} className={classes.infoSection}>
-                    <Col span={4}>고유아이디</Col>
-                    <Col span={18}>{userInfo.id}</Col>
-                    <Col span={4}>계정</Col>
-                    <Col span={18}>{userInfo.userAccount}</Col>
-                    <Col span={4}>학과</Col>
-                    <Col span={20}>{userInfo.department}</Col>
-                    <Col span={4}>학번</Col>
-                    <Col span={20}>{userInfo.studentNumber}</Col>
-                    <Col span={4}>이름</Col>
-                    <Col span={20}>{userInfo.userName}</Col>
-                    <Col span={4}>이메일</Col>
-                    <Col span={20}>{userInfo.email}</Col>
-                    <Col span={4}>상태</Col>
-                    <Col span={20}>
-                        <UserStatus status={userInfo.status} />
-                    </Col>
-                    <Col span={4}>타입</Col>
-                    <Col span={20}>
-                        <UserType type={userInfo.type} />
-                    </Col>
-                </Row>
+                <>
+                    <Row gutter={[12, 12]} className={classes.infoSection}>
+                        <Col span={4}>고유아이디</Col>
+                        <Col span={18}>{userInfo.id}</Col>
+                        <Col span={4}>계정</Col>
+                        <Col span={18}>{userInfo.userAccount}</Col>
+                        <Col span={4}>학과</Col>
+                        <Col span={20}>{userInfo.department}</Col>
+                        <Col span={4}>학번</Col>
+                        <Col span={20}>{userInfo.studentNumber}</Col>
+                        <Col span={4}>이름</Col>
+                        <Col span={20}>{userInfo.userName}</Col>
+                        <Col span={4}>이메일</Col>
+                        <Col span={20}>{userInfo.email}</Col>
+                        <Col span={4}>상태</Col>
+                        <Col span={20}>
+                            <UserStatus status={userInfo.status} />
+                        </Col>
+                        <Col span={4}>타입</Col>
+                        <Col span={20}>
+                            <UserType type={userInfo.type} />
+                        </Col>
+                    </Row>
+                    <Grid container className={classes.buttonSection}>
+                        <Grid item xs={12} sm={2}>
+                            <Button type="primary" danger size="middle" className={classes.button}>
+                                정지
+                            </Button>
+                        </Grid>
+                        {buttonData.map((b) => (
+                            <Grid item xs={12} sm={2}>
+                                <Button
+                                    type="primary"
+                                    size="middle"
+                                    value={b.type}
+                                    className={classes.button}
+                                >
+                                    {b.buttonName}
+                                </Button>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </>
             )}
         </>
     );
