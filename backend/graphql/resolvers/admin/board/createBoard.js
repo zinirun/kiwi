a62274db4763e80f94ebc015e35d3dcd260ca96c
zinirun <heojj97@naver.com>
2021-03-27 18:28:26 +0900

    modify: add logger

--- backend/graphql/resolvers/admin/board/createBoard.js
+++ backend/graphql/resolvers/admin/board/createBoard.js
@@ -5,6 +5,7 @@
 const models = require('../../../../models');
 const isAdmin = require('../../../middlewares/isAdmin');
 const { ConflictError } = require('../../../errors/errors');
+const { createAdminLog } = require('../../../services/log.service');
 
 module.exports = async ({ board }, { id: userId }) => {
     await isAdmin(userId);
@@ -12,6 +13,9 @@ module.exports = async ({ board }, { id: userId }) => {
         .create({
             ...board,
         })
-        .then(() => true)
+        .then(() => {
+            createAdminLog(userId, `[${board.boardName}] 게시판 추가`);
+            return true;
+        })
         .catch(() => ConflictError());
 };
