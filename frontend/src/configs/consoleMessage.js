866b3e9417befae58cae97555282304e2b2b1179
zinirun <heojj97@naver.com>
2021-03-28 16:04:03 +0900

    modify: dev-console

--- frontend/src/configs/consoleMessage.js
+++ frontend/src/configs/consoleMessage.js
@@ -8,8 +8,8 @@ export default function consoleMessage() {
         'color:dodgerblue; font-size:23px; margin-bottom: 10px;',
     );
     console.log(
-        '%c멈춰!\n%c 이 콘솔은 개발자용으로 브라우저에서 제공됩니다. 누군가 코드를 복사하여 붙여넣으라고 했다면 100% 사기 행위입니다. 회원님의 접근 권한을 해커에게 전부 부여할 수 있습니다. 관련 행위는 <키위 신고>를 통해 신고해주세요.',
-        'margin-top:5px;background-color:crimson; color:#ffffff; font-size:24px;',
-        'margin-top:10px;font-size:16px;',
+        '%c멈춰!\n%c이 콘솔은 개발자용으로 브라우저에서 제공됩니다. 누군가 코드를 복사하여 붙여넣으라고 했다면 100% 사기 행위입니다. 회원님의 접근 권한을 해커에게 전부 부여할 수 있습니다. 관련 행위는 <키위 신고>를 통해 신고해주세요.',
+        'margin-top:10px;background-color:crimson; color:#ffffff; font-size:24px;',
+        'margin:10px 0;font-size:16px;',
     );
 }
