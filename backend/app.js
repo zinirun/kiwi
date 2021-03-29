c5a62c625e9fbc9124f80edcd6180a2bb548e43a
zinirun <heojj97@naver.com>
2021-03-29 19:34:07 +0900

    fix: add bodyParser

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const db = require('./models');
@@ -45,6 +46,8 @@ class App {
         // this.app.use(hpp());
         this.app.use(logger('dev'));
         this.app.use(cookieParser());
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(cors());
     }
 
