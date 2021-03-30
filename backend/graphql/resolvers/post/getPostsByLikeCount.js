5188fed5008031f55409d0990dce3b4216aa3f4b
zinirun <heojj97@naver.com>
2021-03-30 21:58:23 +0900

    add: resolver

---
+++ backend/graphql/resolvers/post/getPostsByLikeCount.js
@@ -0,0 +1,68 @@
+/**
+ * 좋아요 갯수 별 게시물 추출
+ * @author 이건욱
+ * @param (likeCount: Int!)
+ * @returns {[PostList]}
+ * type PostList {
+        id: ID!
+        title: String!
+        gradeName: String!
+        authorName: String!
+        updatedAt: Date!
+        categoryName: String
+        likeCount: Int!
+        commentCount: Int!
+    }
+
+* getPostsByLikecount(likeCount: Int!): [PostList]
+ */
+
+const models = require('../../../models');
+const { ConflictError } = require('../../errors/errors');
+
+module.exports = async ({ likeCount, pageNumber, elementCount }, { departmentId }) => {
+    const query = `
+                    select p.id,
+                        p.title,
+                        g.gradeName,
+                        u.userName as authorName,
+                        p.createdAt,
+                        p.updatedAt,
+                        cg.categoryName,
+                        ifnull(v.postLikeCount, 0) as likeCount,
+                        ifnull(z.commentCount, 0)  as commentCount
+                    from post p
+                        left join category cg on p.categoryId = cg.id
+                        left join (select c.id, count(c.id) as commentCount, c.postId
+                                    from comment c
+                                    where c.isDeleted = 0
+                                    group by c.postId) as z on p.id = z.postId
+                        left join (select pl.postId, count(pl.id) as postLikeCount
+                                    from post_like pl
+                                    where pl.isDeleted = 0
+                                    group by pl.postId) as v on p.id = v.postId,
+                        user u
+                        left join grade g on u.studentGradeId = g.id
+                    where p.authorId = u.id
+                    and p.isDeleted = 0
+                    and ifnull(v.postLikeCount, 0) >= :likeCount
+                    and p.departmentId = :departmentId
+                    order by p.id desc
+                    limit :pages, :elementCount;
+                    `;
+    return await models.sequelize
+        .query(query, {
+            replacements: {
+                likeCount,
+                departmentId,
+                pages: elementCount * (+pageNumber - 1),
+                elementCount,
+            },
+        })
+        .spread(
+            (result) => {
+                return JSON.parse(JSON.stringify(result));
+            },
+            () => ConflictError('Database error'),
+        );
+};
