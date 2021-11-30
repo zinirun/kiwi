fbc2d9bd5f488419be0d8ef947523ff46603dad2
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-11-30 21:23:50 +0900

    modify: domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="kiwi.nemobros.com"
+domains="zzangwoo.shop"
 rsa_key_size=4096
 data_path="../certbot"
 email="zrunkr@gmail.com" # Adding a valid address is strongly recommended
